88d5a1b884898deab58d5da4ff372b4ea680b8ac
Leonam Sousa <leonamgm18@gmail.com>
2022-07-08 23:58:28 -0300

    Implementado classe BLLProduto

---
+++ MyStore/BBL/BLLProduto.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Modelo;
+
+namespace BBL
+{
+    public class BLLProduto
+    {
+        public DALConexao conexao { get; set; }
+
+        public BLLProduto(DALConexao cx)
+        {
+            conexao = cx;
+        }
+
+        public void Incluir(ModeloProduto _modelo)
+        {
+            if (_modelo.Pro_nome.Trim().Length == 0)
+            {
+                throw new Exception("Nome Obrigatório");
+            }
+            if (_modelo.Pro_descricao.Trim().Length == 0)
+            {
+                throw new Exception("Descrição Obrigatória");
+            }
+            if (_modelo.Pro_valorVenda <= 0)
+            {
+                throw new Exception("Valor Obrigatório");
+            }
+            if (_modelo.Pro_quantidade < 0)
+            {
+                throw new Exception("Informar quantidade maior ou igual a zero");
+            }
+            if (_modelo.Umed_cod <= 0)
+            {
+                throw new Exception("O codigo é Obrigatório");
+            }
+            if (_modelo.Cat_cod <= 0)
+            {
+                throw new Exception("O codigo é Obrigatório");
+            }
+            if (_modelo.Scat_cod <= 0)
+            {
+                throw new Exception("O codigo é Obrigatório");
+            }
+            DALProduto DALobj = new DALProduto(conexao);
+            DALobj.Incluir(_modelo);
+        }
+
+        public void Alterar(ModeloProduto _modelo)
+        {
+            if (_modelo.Pro_cod <= 0)
+            {
+                throw new Exception("Codigo Obrigatório");
+            }
+            if (_modelo.Pro_nome.Trim().Length == 0)
+            {
+                throw new Exception("Nome Obrigatório");
+            }
+            if (_modelo.Pro_descricao.Trim().Length == 0)
+            {
+                throw new Exception("Descrição Obrigatória");
+            }
+            if (_modelo.Pro_valorVenda <= 0)
+            {
+                throw new Exception("Valor Obrigatório");
+            }
+            if (_modelo.Pro_quantidade < 0)
+            {
+                throw new Exception("Informar quantidade maior ou igual a zero");
+            }
+            if (_modelo.Umed_cod <= 0)
+            {
+                throw new Exception("O codigo é Obrigatório");
+            }
+            if (_modelo.Cat_cod <= 0)
+            {
+                throw new Exception("O codigo é Obrigatório");
+            }
+            if (_modelo.Scat_cod <= 0)
+            {
+                throw new Exception("O codigo é Obrigatório");
+            }
+            DALProduto DALobj = new DALProduto(conexao);
+            DALobj.Alterar(_modelo);
+        }
+
+        public void Excluir(int _codigo)
+        {
+            DALProduto DALobj = new DALProduto(conexao);
+            DALobj.Excluir(_codigo);
+        }
+
+        public DataTable Localizar(String _value)
+        {
+            DALProduto DALobj = new DALProduto(conexao);
+            return DALobj.Localizar(_value);
+        }
+
+        public ModeloProduto CarregaModeloProduto(int _codigo)
+        {
+            DALProduto DALobj = new DALProduto(conexao);
+            return DALobj.CarregaModeloProduto(_codigo);
+        }
+    }
+}
